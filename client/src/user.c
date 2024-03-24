c955174a08545b91d3178d4cccf402da9efff8c6
adm <xdxddd932@gmail.com>
2024-03-24 16:33:32 -0700

    rzhaka anegdoti++

--- client/src/user.c
+++ client/src/user.c
@@ -16,17 +16,11 @@ void on_window_realize(GtkWidget *widget, gpointer data) {
     // g_print("%d\n", selected_user.index);
     // Проверяем, выбран ли пользователь
 }
-static void logout_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("Logout clicked\n");
+
+void on_window_realize_2(GtkWidget *widget, gpointer data) {
+    gtk_widget_hide(widget);
 }
 
-static void settings_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("Settings clicked\n");
-}
-
-static void user_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("User clicked\n");
-}
 static void message_search_clicked(GtkWidget *widget, gpointer data) {
     g_print("Message search clicked\n");
 }
@@ -40,9 +34,14 @@ static void toggle(GtkWidget *widget, GtkWidget *element) {
     gtk_widget_set_visible(element, !visible);
 }
 
-static void show_side(GtkWidget *widget, gpointer data){
-    g_print("User clicked\n");
+static void clicked_side(GtkWidget *widget, gpointer data){
+    g_print("side clicked\n");
 }
+
+static void clicked_settings(GtkWidget *widget, gpointer data){
+    g_print("settings clicked\n");
+}
+
 
 void draw_user_window() {
     GtkCssProvider *cssProvider = gtk_css_provider_new();
@@ -79,43 +78,88 @@ void draw_user_window() {
     // gtk_widget_set_visible(sidebar, FALSE);
 
     GtkWidget *side_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-    gtk_widget_override_background_color(side_box, GTK_STATE_FLAG_NORMAL, &(GdkRGBA){DARK_GRAY, DARK_GRAY, DARK_GRAY, 1.0});
+    gtk_widget_override_background_color(side_box, GTK_STATE_FLAG_NORMAL, NORM_CVET);
     gtk_widget_set_size_request(side_box, 75, -1);
     //gtk_widget_set_visible(sidebar, FALSE);
     
     //endsidebar
 
+    // GtkWidget* add = gtk_button_new();
+    // gtk_widget_set_valign(GTK_WIDGET(add), GTK_ALIGN_CENTER);
+    // gtk_button_set_relief(GTK_BUTTON(add), GTK_RELIEF_NONE);
+    // gtk_container_set_border_width(GTK_CONTAINER(add), 0);
+    // // gtk_widget_set_halign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    // gtk_widget_set_size_request(GTK_WIDGET(add), 64, 64);
+    // gtk_widget_set_name(GTK_WIDGET(add), "add");
+    // g_signal_connect(G_OBJECT(add), "realize", G_CALLBACK(on_window_realize_2), NULL);
+
+    // GtkWidget* delete = gtk_button_new();
+    // gtk_widget_set_valign(GTK_WIDGET(delete), GTK_ALIGN_CENTER);
+    // gtk_button_set_relief(GTK_BUTTON(delete), GTK_RELIEF_NONE);
+    // gtk_container_set_border_width(GTK_CONTAINER(delete), 0);
+    // // gtk_widget_set_halign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    // gtk_widget_set_size_request(GTK_WIDGET(delete), 64, 64);
+    // gtk_widget_set_name(GTK_WIDGET(delete), "delete");
+    // g_signal_connect(G_OBJECT(delete), "realize", G_CALLBACK(on_window_realize_2), NULL);
+
+    GtkWidget* anekdot = gtk_button_new();
+    gtk_widget_set_valign(GTK_WIDGET(anekdot), GTK_ALIGN_CENTER);
+    gtk_button_set_relief(GTK_BUTTON(anekdot), GTK_RELIEF_NONE);
+    gtk_container_set_border_width(GTK_CONTAINER(anekdot), 0);
+    // gtk_widget_set_halign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    gtk_widget_set_size_request(GTK_WIDGET(anekdot), 64, 64);
+    gtk_widget_set_name(GTK_WIDGET(anekdot), "anekdot");
+    g_signal_connect(G_OBJECT(anekdot), "realize", G_CALLBACK(on_window_realize_2), NULL);
+
+    GtkWidget* account = gtk_button_new();
+    gtk_widget_set_valign(GTK_WIDGET(account), GTK_ALIGN_CENTER);
+    gtk_button_set_relief(GTK_BUTTON(account), GTK_RELIEF_NONE);
+    gtk_container_set_border_width(GTK_CONTAINER(account), 0);
+    // gtk_widget_set_halign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    gtk_widget_set_size_request(GTK_WIDGET(account), 64, 64);
+    gtk_widget_set_name(GTK_WIDGET(account), "account");
+    g_signal_connect(G_OBJECT(account), "realize", G_CALLBACK(on_window_realize_2), NULL);
+
     GtkWidget* settings_img = gtk_button_new();
     gtk_widget_set_valign(GTK_WIDGET(settings_img), GTK_ALIGN_CENTER);
     gtk_button_set_relief(GTK_BUTTON(settings_img), GTK_RELIEF_NONE);
     gtk_container_set_border_width(GTK_CONTAINER(settings_img), 0);
-    // gtk_widget_set_halign(GTK_WIDGET(settings_img), GTK_ALIGN_CENTER);
     gtk_widget_set_size_request(GTK_WIDGET(settings_img), 64, 64);
     gtk_widget_set_name(GTK_WIDGET(settings_img), "settings");
-    g_signal_connect(G_OBJECT(settings_img), "clicked", G_CALLBACK(settings_button_clicked), NULL);
-    g_signal_connect(G_OBJECT(settings_img), "clicked", G_CALLBACK(show_side), NULL);
+    g_signal_connect(G_OBJECT(settings_img), "clicked", G_CALLBACK(clicked_settings), NULL);
+    g_signal_connect(G_OBJECT(settings_img), "realize", G_CALLBACK(on_window_realize_2), NULL);
 
-    // GtkWidget* user_img = gtk_button_new();
-    // gtk_widget_set_valign(GTK_WIDGET(user_img), GTK_ALIGN_CENTER);
-    // gtk_button_set_relief(GTK_BUTTON(user_img), GTK_RELIEF_NONE);
-    // gtk_container_set_border_width(GTK_CONTAINER(user_img), 0);
-    // // gtk_widget_set_halign(GTK_WIDGET(settings_img), GTK_ALIGN_CENTER);
-    // gtk_widget_set_size_request(GTK_WIDGET(user_img), 64, 64);
-    // gtk_widget_set_name(GTK_WIDGET(user_img), "user");
-    // g_signal_connect(G_OBJECT(user_img), "clicked", G_CALLBACK(user_button_clicked), NULL);
+    GtkWidget* side_img = gtk_button_new();
+    gtk_widget_set_valign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    gtk_button_set_relief(GTK_BUTTON(side_img), GTK_RELIEF_NONE);
+    gtk_container_set_border_width(GTK_CONTAINER(side_img), 0);
+    // gtk_widget_set_halign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    gtk_widget_set_size_request(GTK_WIDGET(side_img), 64, 64);
+    gtk_widget_set_name(GTK_WIDGET(side_img), "sidebar");
+    g_signal_connect(G_OBJECT(side_img), "clicked", G_CALLBACK(toggle), settings_img);
+    g_signal_connect(G_OBJECT(side_img), "clicked", G_CALLBACK(toggle), anekdot);
+    g_signal_connect(G_OBJECT(side_img), "clicked", G_CALLBACK(toggle), account);
+    // g_signal_connect(G_OBJECT(side_img), "clicked", G_CALLBACK(toggle), add);
+    // g_signal_connect(G_OBJECT(side_img), "clicked", G_CALLBACK(toggle), delete);
+    g_signal_connect(G_OBJECT(side_img), "clicked", G_CALLBACK(clicked_side), NULL);
 
-    // GtkWidget* logout_img = gtk_button_new();
-    // gtk_widget_set_valign(GTK_WIDGET(logout_img), GTK_ALIGN_CENTER);
-    // gtk_button_set_relief(GTK_BUTTON(logout_img), GTK_RELIEF_NONE);
-    // gtk_container_set_border_width(GTK_CONTAINER(logout_img), 0);
-    // // gtk_widget_set_halign(GTK_WIDGET(settings_img), GTK_ALIGN_CENTER);
-    // gtk_widget_set_size_request(GTK_WIDGET(logout_img), 64, 64);
-    // gtk_widget_set_name(GTK_WIDGET(logout_img), "logout");
-    // g_signal_connect(G_OBJECT(logout_img), "clicked", G_CALLBACK(logout_button_clicked), NULL);
+    GtkWidget* logout_img = gtk_button_new();
+    gtk_widget_set_valign(GTK_WIDGET(logout_img), GTK_ALIGN_CENTER);
+    gtk_button_set_relief(GTK_BUTTON(logout_img), GTK_RELIEF_NONE);
+    gtk_container_set_border_width(GTK_CONTAINER(logout_img), 0);
+    // gtk_widget_set_halign(GTK_WIDGET(side_img), GTK_ALIGN_CENTER);
+    gtk_widget_set_size_request(GTK_WIDGET(logout_img), 64, 64);
+    gtk_widget_set_name(GTK_WIDGET(logout_img), "logout");
+    g_signal_connect(G_OBJECT(logout_img), "clicked", G_CALLBACK(clicked_side), NULL);
 
+    
+    gtk_box_pack_start(GTK_BOX(side_box), side_img, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(side_box), settings_img, FALSE, FALSE, 0);
-    // gtk_box_pack_start(GTK_BOX(side_bar_box), user_img, FALSE, FALSE, 0);
-    // gtk_box_pack_end(GTK_BOX(side_bar_box), logout_img, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(side_box), account, FALSE, FALSE, 0);
+    // gtk_box_pack_start(GTK_BOX(side_box), add, FALSE, FALSE, 0);
+    // gtk_box_pack_start(GTK_BOX(side_box), delete, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(side_box), anekdot, FALSE, FALSE, 0);
+    gtk_box_pack_end(GTK_BOX(side_box), logout_img, FALSE, FALSE, 0);
 
     // Create a search bar
     GtkWidget *search_entry = gtk_entry_new();
@@ -128,6 +172,7 @@ void draw_user_window() {
     // Create the button
     GtkWidget *search_button = gtk_button_new_with_label("[ Find a user ]");
     gtk_widget_set_size_request(search_button, 25, 35);
+    gtk_widget_set_name(GTK_WIDGET(search_button), "search_button");
 
     // Pack the search box and margin into a vertical box container
     gtk_box_pack_start(GTK_BOX(search_box), search_button, FALSE, FALSE, 0);
